0973ed9dc7b258342f176471045a6725b7d40a49
kdmdk <hzmd8274@gmial.com>
2018-03-02 23:00:13 +0900

    StepAsideUnityChan　１回目
    アイテム削除と動的生成ができました

---
+++ Assets/destroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour {
+
+	private GameObject unitychan;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.unitychan = GameObject.Find ("unitychan");
+		if (unitychan.transform.position.z - 5 > this.transform.position.z) {
+			Destroy (this.gameObject);
+		}
+	}
+}
